815bd10c97465c093931e641b94a37d275b76991
Manuel Lopez <jmlopez@iofficecorp.com>
2016-01-04 14:50:25 -0600

    eslint

--- dist/src.js
+++ dist/src.js
@@ -1,86 +1,108 @@
 'use strict';
 
-var query = {};
-var map = {};
+function getQueryParams() {
+  var url = arguments.length <= 0 || arguments[0] === undefined ? window.location.href : arguments[0];
 
-var url = window.location.href;
-
-url = url.substr(url.lastIndexOf('?') + 1).split('&');
-_.each(url, function (item) {
-  var line = item.split('=');
-  query[decodeURIComponent(line[0])] = decodeURIComponent(line[1]);
-  map[decodeURIComponent(line[0])] = decodeURIComponent(line[1]);
-});
-
-document.getElementsByTagName('fv-map')[0].setAttribute('floor-id', parseInt(map['id']));
-document.getElementById('title').innerHTML = map['name'];
-
-map = {};
-//22miles
-window.m2RunScript = function (str) {
-  str = escape(str) + ' _unsetup=null;/**/';
-  window.location = 'run://' + str;
-};
-
-//from TD
-function zoomIn(e) {
-  M2.zoomIn(e);
-}
-function zoomOut(e) {
-  M2.zoomOut(e);
-}
-function reset(e) {
-  M2.reset();
-}
-function selectRoom(id) {
-  map.selectRoom.byId(id);
+  var query = {};
+  var params = url.substr(url.lastIndexOf('?') + 1).split('&');
+  _.each(params, function (item) {
+    var col = item.split('=');
+    query[decodeURIComponent(col[0])] = decodeURIComponent(col[1]);
+  });
+  return query;
 }
 
-var M2 = {
-  position: {},
-  roomID: '',
+function setFloorInfo(query) {
+  var fvEle = document.getElementsByTagName('fv-map')[0];
+  var titleEle = document.getElementById('title');
+  fvEle.setAttribute('floor-id', parseInt(query.id, 10));
+  titleEle.innerHTML = query.name;
+}
+
+var query = getQueryParams();
+setFloorInfo(query);
+
+// GLOBALS
+_.assign(window, {
+  gMap: null,
+  M2: {
+    position: {},
+    roomID: '',
+    zoomIn: function zoomIn(e) {
+      if (gMap) {
+        gMap.zoomIn(e);
+      }
+    },
+    zoomOut: function zoomOut(e) {
+      if (gMap) {
+        gMap.zoomOut(e);
+      }
+    },
+    reset: function reset() {
+      if (gMap) {
+        gMap.panTo(M2.position.center, { animate: false });
+        gMap.setZoom(M2.position.zoom);
+        if (M2.roomID) {
+          gMap.selectRoom.byName('');
+          gMap.options.selectMode = false;
+          gMap._refreshLayers();
+          M2.roomID = '';
+          try {
+            m2SetRTVar('roomId', '');
+          } catch (er) {
+            console.log('m2SetRTVar Error...');
+          }
+        }
+      }
+    },
+    onRoomClick: function onRoomClick(e) {
+      var room = e.room;
+      var id = room.id;
+      if (M2.roomID !== id) {
+        M2.roomID = id;
+        try {
+          m2SetRTVar('roomId', '' + id);
+        } catch (er) {
+          console.log('m2SetRTVar Error...');
+        }
+        if (room.options.interactive) {
+          gMap.selectRoom.byId(room.id);
+          gMap.pingRoom(room, {
+            screenRatio: 0.1,
+            pulses: 4
+          });
+        }
+      }
+    }
+  },
+  m2RunScript: function m2RunScript(str) {
+    // 22 Miles
+    var result = escape(str) + ' _unsetup=null;/**/';
+    window.location = 'run://' + result;
+  },
   zoomIn: function zoomIn(e) {
-    if (map) map.zoomIn(e);
+    M2.zoomIn(e);
   },
   zoomOut: function zoomOut(e) {
-    if (map) map.zoomOut(e);
+    M2.zoomOut(e);
   },
   reset: function reset() {
-    if (map) {
-      map.panTo(M2.position.center, { animate: false });
-      map.setZoom(M2.position.zoom);
-      if (M2.roomID) {
-        map.selectRoom.byName('');
-        map.options.selectMode = false;
-        map._refreshLayers();
-        M2.roomID = '';
-        try {
-          m2SetRTVar('roomId', '');
-        } catch (er) {}
-      }
-    }
+    M2.reset();
   },
-  onRoomClick: function onRoomClick(e) {
-    var room = e.room;
-    var id = room.id;
-    if (M2.roomID !== id) {
-      M2.roomID = id;
-      try {
-        m2SetRTVar('roomId', id + '');
-      } catch (er) {}
-      if (room.options.interactive) {
-        map.selectRoom.byId(room.id);
-        map.pingRoom(room, {
-          screenRatio: .1,
-          pulses: 10
-        });
-      }
+  selectRoom: function selectRoom(id) {
+    if (gMap) {
+      gMap.selectRoom.byId(id);
+      gMap.pingRoom(gMap.roomById[id], {
+        screenRatio: 0.1,
+        pulses: 4
+      });
     }
   }
-};
+});
 
-function addMarker(map, room, icon, popupLabel, delta) {
-  delta = delta || [0, 0];
+function addMarker(map, room, icon, popupLabel) {
+  var delta = arguments.length <= 4 || arguments[4] === undefined ? [0, 0] : arguments[4];
+
   var b = room.getLargestRectangleBounds();
   var marker = L.marker([b.getNorth() + delta[1], b.getCenter().lng + delta[0]], { icon: icon });
   if (popupLabel) {
@@ -90,16 +112,16 @@ function addMarker(map, room, icon, popupLabel, delta) {
 }
 
 L.control({ 'position': 'topright' });
-angular.module('DemoApp', ['i.floor-viewer']).controller('DemoController', function ($scope, fvUtil, fvDelegate) {
+angular.module('DemoApp', ['i.floor-viewer']).controller('DemoController', function DemoController($scope, fvUtil, fvDelegate) {
   fvUtil.config('https://demo.iofficeconnect.com', {
     'x-auth-username': query.user,
     'x-auth-password': query.pass
   });
   setTimeout(function () {
     fvDelegate.getMap().then(function (map) {
-      this.map = map;
+      gMap = map;
       map.zoomControl.setPosition('topright');
-      //map.setZoom(map.getZoom() + 1);
+      // map.setZoom(map.getZoom() + 1);
       map.on('roomclick', M2.onRoomClick);
       document.getElementById('title').innerHTML = map.floor.building.name + ' / ' + map.floor.name;
       M2.position.zoom = map.getZoom();
@@ -110,33 +132,32 @@ angular.module('DemoApp', ['i.floor-viewer']).controller('DemoController', funct
       }
 
       map.onRoomsReady.then(function () {
-
         L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
         var menRestRoomIcon = L.AwesomeMarkers.icon({
           icon: 'man',
           markerColor: 'gray'
-        }),
-            womenRestRoomIcon = L.AwesomeMarkers.icon({
+        });
+        var womenRestRoomIcon = L.AwesomeMarkers.icon({
           icon: 'woman',
           markerColor: 'gray'
-        }),
-            foodIcon = L.AwesomeMarkers.icon({
+        });
+        var foodIcon = L.AwesomeMarkers.icon({
           icon: 'pizza',
           markerColor: 'blue'
-        }),
-            hereIcon = L.AwesomeMarkers.icon({
+        });
+        var hereIcon = L.AwesomeMarkers.icon({
           icon: 'home',
           markerColor: 'red'
-        }),
-            labIcon = L.AwesomeMarkers.icon({
+        });
+        var labIcon = L.AwesomeMarkers.icon({
           icon: 'erlenmeyer-flask',
           markerColor: 'purple'
-        }),
-            exitIcon = L.AwesomeMarkers.icon({
+        });
+        var exitIcon = L.AwesomeMarkers.icon({
           icon: 'android-exit',
           markerColor: 'red'
         });
-        //22miles
+        // 22miles
         try {
           var foodLayer = addMarker(map, map.roomByName['1834'], foodIcon, 'Cafeteria', [0, -0.3]);
           var hereLayer = addMarker(map, map.roomByName['1819'], hereIcon, 'You Are Here', [0.2, -0.85]);
@@ -154,11 +175,13 @@ angular.module('DemoApp', ['i.floor-viewer']).controller('DemoController', funct
           };
           var panelLayers = L.control.layers(baseLayers, overLayers);
           map.addControl(panelLayers);
-        } catch (er) {}
+        } catch (er) {
+          console.log('Error adding layers...');
+        }
         var legend = L.control({ position: 'bottomright' });
-        legend.onAdd = function (map) {
-          var div = L.DomUtil.create('div', 'info legend'),
-              data = {
+        legend.onAdd = function () {
+          var div = L.DomUtil.create('div', 'info legend');
+          var data = {
             'Non-Reservable': 'lightgray',
             'Reservable': '#80C680',
             'Partially Available': '#FFB74D',
--- src/src.es6.js
+++ src/src.es6.js
@@ -1,178 +1,208 @@
-const query = {};
-let map = {};
-
-let url = window.location.href;
-
-url = url.substr(url.lastIndexOf('?') + 1).split('&');
-_.each(url, (item) => {
-  const line = item.split('=');
-  query[decodeURIComponent(line[0])] = decodeURIComponent(line[1]);
-  map[decodeURIComponent(line[0])] = decodeURIComponent(line[1]);
-});
-
-document.getElementsByTagName('fv-map')[0].setAttribute('floor-id', parseInt(map['id']));
-document.getElementById('title').innerHTML = map['name'];
-
-map = {};
-//22miles
-window.m2RunScript = function(str)
-{
-  str = escape(str)+' _unsetup=null;/**/';
-  window.location = 'run://' + str;
-};
-
-//from TD
-function zoomIn(e) { M2.zoomIn(e); }
-function zoomOut(e) { M2.zoomOut(e); }
-function reset(e) { M2.reset(); }
-function selectRoom(id) {
-  map.selectRoom.byId(id);
+function getQueryParams(url = window.location.href) {
+  const query = {};
+  const params = url.substr(url.lastIndexOf('?') + 1).split('&');
+  _.each(params, (item) => {
+    const col = item.split('=');
+    query[decodeURIComponent(col[0])] = decodeURIComponent(col[1]);
+  });
+  return query;
 }
 
-var M2 = {
-  position: {},
-  roomID: '',
-  zoomIn: function(e) { if (map) map.zoomIn(e); },
-  zoomOut: function(e) { if (map) map.zoomOut(e); },
-  reset: function() {
-    if (map) {
-      map.panTo(M2.position.center, {animate:false});
-      map.setZoom(M2.position.zoom);
-      if (M2.roomID) {
-        map.selectRoom.byName('');
-        map.options.selectMode = false;
-        map._refreshLayers();
-        M2.roomID = '';
-        try {
-          m2SetRTVar('roomId', '');
-        } catch (er) {}
+function setFloorInfo(query) {
+  const fvEle = document.getElementsByTagName('fv-map')[0];
+  const titleEle = document.getElementById('title');
+  fvEle.setAttribute('floor-id', parseInt(query.id, 10));
+  titleEle.innerHTML = query.name;
+}
+
+
+const query = getQueryParams();
+setFloorInfo(query);
+
+
+// GLOBALS
+_.assign(window, {
+  gMap: null,
+  M2: {
+    position: {},
+    roomID: '',
+    zoomIn(e) {
+      if (gMap) {
+        gMap.zoomIn(e);
       }
+    },
+    zoomOut(e) {
+      if (gMap) {
+        gMap.zoomOut(e);
+      }
+    },
+    reset() {
+      if (gMap) {
+        gMap.panTo(M2.position.center, {animate: false});
+        gMap.setZoom(M2.position.zoom);
+        if (M2.roomID) {
+          gMap.selectRoom.byName('');
+          gMap.options.selectMode = false;
+          gMap._refreshLayers();
+          M2.roomID = '';
+          try {
+            m2SetRTVar('roomId', '');
+          } catch (er) {
+            console.log('m2SetRTVar Error...');
+          }
+        }
+      }
+    },
+    onRoomClick(e) {
+      const room = e.room;
+      const id = room.id;
+      if (M2.roomID !== id) {
+        M2.roomID = id;
+        try {
+          m2SetRTVar('roomId', '' + id);
+        } catch (er) {
+          console.log('m2SetRTVar Error...');
+        }
+        if (room.options.interactive) {
+          gMap.selectRoom.byId(room.id);
+          gMap.pingRoom(room, {
+            screenRatio: 0.1,
+            pulses: 4,
+          });
+        }
+      }
+    },
+  },
+  m2RunScript(str) {
+    // 22 Miles
+    const result = escape(str) + ' _unsetup=null;/**/';
+    window.location = 'run://' + result;
+  },
+  zoomIn(e) {
+    M2.zoomIn(e);
+  },
+  zoomOut(e) {
+    M2.zoomOut(e);
+  },
+  reset() {
+    M2.reset();
+  },
+  selectRoom(id) {
+    if (gMap) {
+      gMap.selectRoom.byId(id);
+      gMap.pingRoom(gMap.roomById[id], {
+        screenRatio: 0.1,
+        pulses: 4,
+      });
     }
   },
-  onRoomClick: function(e) {
-    var room = e.room;
-    var id = room.id;
-    if (M2.roomID !== id) {
-      M2.roomID = id;
-      try {
-        m2SetRTVar('roomId', id + '');
-      } catch (er) {}
-      if (room.options.interactive) {
-        map.selectRoom.byId(room.id);
-        map.pingRoom(room, {
-          screenRatio: .1,
-          pulses: 10
-        });
-      }
-    }
-  }
-};
+});
 
-function addMarker(map, room, icon, popupLabel, delta) {
-  delta = delta || [0, 0];
-  var b = room.getLargestRectangleBounds();
-  var marker = L.marker([b.getNorth() + delta[1], b.getCenter().lng + delta[0]], {icon: icon});
+
+function addMarker(map, room, icon, popupLabel, delta = [0, 0]) {
+  const b = room.getLargestRectangleBounds();
+  const marker = L.marker(
+    [b.getNorth() + delta[1], b.getCenter().lng + delta[0]],
+    {icon: icon}
+  );
   if (popupLabel) {
     marker.bindPopup(popupLabel);
   }
   return marker.addTo(map);
 }
 
-L.control({'position':'topright'});
+L.control({'position': 'topright'});
 angular.module('DemoApp', ['i.floor-viewer'])
-    .controller('DemoController', function($scope, fvUtil, fvDelegate) {
-      fvUtil.config('https://demo.iofficeconnect.com', {
-        'x-auth-username': query.user,
-        'x-auth-password': query.pass,
-      });
-      setTimeout(function() {
-        fvDelegate.getMap().then(function(map) {
-          this.map = map;
-          map.zoomControl.setPosition('topright');
-          //map.setZoom(map.getZoom() + 1);
-          map.on('roomclick', M2.onRoomClick);
-          document.getElementById('title').innerHTML = map.floor.building.name + ' / ' + map.floor.name;
-          M2.position.zoom = map.getZoom();
-          M2.position.center = map.getCenter();
-
-          if (map.floor.id !== 224) {
-            return;
-          }
-
-
-
-          map.onRoomsReady.then(function() {
-
-            L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
-            var menRestRoomIcon = L.AwesomeMarkers.icon({
-                icon: 'man',
-                markerColor: 'gray'
-              }),
-              womenRestRoomIcon = L.AwesomeMarkers.icon({
-              icon: 'woman',
-              markerColor: 'gray'
-            }),
-              foodIcon = L.AwesomeMarkers.icon({
-              icon: 'pizza',
-              markerColor: 'blue'
-            }),
-              hereIcon = L.AwesomeMarkers.icon({
-              icon: 'home',
-              markerColor: 'red'
-            }),
-              labIcon = L.AwesomeMarkers.icon({
-              icon: 'erlenmeyer-flask',
-              markerColor: 'purple'
-            }),
-              exitIcon = L.AwesomeMarkers.icon({
-              icon: 'android-exit',
-              markerColor: 'red'
-            });
-            //22miles
-            try {
-              var foodLayer = addMarker(map, map.roomByName['1834'], foodIcon, 'Cafeteria', [0, -0.3]);
-              var hereLayer = addMarker(map, map.roomByName['1819'], hereIcon, 'You Are Here', [0.2, -0.85]);
-              var restroomsLayers = new L.LayerGroup([
-                addMarker(map, map.roomByName['1832'], womenRestRoomIcon, 'Women'),
-                addMarker(map, map.roomByName['1833'], menRestRoomIcon, 'Men')
-              ]).addTo(map);
-              var labLayer = addMarker(map, map.roomByName['1822'], labIcon, 'Laboratory', [0, -0.1]);
-              var exitLayer = new L.LayerGroup([
-                addMarker(map, map.roomByName['1826'], exitIcon, 'Emergency Exit', [0.1, -1.725]),
-                addMarker(map, map.roomByName['1803'], exitIcon, 'Emergency Exit', [-0.025, 1.3])
-              ]).addTo(map);
-
-              var baseLayers = {};
-              var overLayers = {
-                '<i class="icon ion-home"></i> You Are Here': hereLayer,
-                '<i class="icon ion-woman"></i><i class="icon ion-man"></i> Restrooms': restroomsLayers,
-                '<i class="icon ion-pizza"></i> Cafeteria': foodLayer,
-                '<i class="icon ion-erlenmeyer-flask"></i> Laboratory': labLayer,
-                '<i class="icon ion-android-exit"></i> Emergency Exit': exitLayer
-              };
-              var panelLayers = L.control.layers(baseLayers, overLayers);
-              map.addControl(panelLayers);
-            } catch (er) {}
-            var legend = L.control({position: 'bottomright'});
-            legend.onAdd = function(map) {
-              var div = L.DomUtil.create('div', 'info legend'),
-                data = {
-                  'Non-Reservable': 'lightgray',
-                  'Reservable': '#80C680',
-                  'Partially Available': '#FFB74D',
-                  'At Capacity': '#D32F2F'
-                };
-              _.each(data, function(color, label) {
-                  div.innerHTML +=
-                    '<i style="background:' + color + '"></i> ' +
-                    label + '<br>';
-                });
-              return div;
-            };
-            legend.addTo(map);
-
-          });
-        });
-      }, 30);
-
+  .controller('DemoController', function DemoController($scope, fvUtil, fvDelegate) {
+    fvUtil.config('https://demo.iofficeconnect.com', {
+      'x-auth-username': query.user,
+      'x-auth-password': query.pass,
     });
+    setTimeout(() => {
+      fvDelegate.getMap().then((map) => {
+        gMap = map;
+        map.zoomControl.setPosition('topright');
+        // map.setZoom(map.getZoom() + 1);
+        map.on('roomclick', M2.onRoomClick);
+        document.getElementById('title').innerHTML = map.floor.building.name + ' / ' + map.floor.name;
+        M2.position.zoom = map.getZoom();
+        M2.position.center = map.getCenter();
+
+        if (map.floor.id !== 224) {
+          return;
+        }
+
+        map.onRoomsReady.then(() => {
+          L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
+          const menRestRoomIcon = L.AwesomeMarkers.icon({
+            icon: 'man',
+            markerColor: 'gray',
+          });
+          const womenRestRoomIcon = L.AwesomeMarkers.icon({
+            icon: 'woman',
+            markerColor: 'gray',
+          });
+          const foodIcon = L.AwesomeMarkers.icon({
+            icon: 'pizza',
+            markerColor: 'blue',
+          });
+          const hereIcon = L.AwesomeMarkers.icon({
+            icon: 'home',
+            markerColor: 'red',
+          });
+          const labIcon = L.AwesomeMarkers.icon({
+            icon: 'erlenmeyer-flask',
+            markerColor: 'purple',
+          });
+          const exitIcon = L.AwesomeMarkers.icon({
+            icon: 'android-exit',
+            markerColor: 'red',
+          });
+          // 22miles
+          try {
+            const foodLayer = addMarker(map, map.roomByName['1834'], foodIcon, 'Cafeteria', [0, -0.3]);
+            const hereLayer = addMarker(map, map.roomByName['1819'], hereIcon, 'You Are Here', [0.2, -0.85]);
+            const restroomsLayers = new L.LayerGroup([
+              addMarker(map, map.roomByName['1832'], womenRestRoomIcon, 'Women'),
+              addMarker(map, map.roomByName['1833'], menRestRoomIcon, 'Men'),
+            ]).addTo(map);
+            const labLayer = addMarker(map, map.roomByName['1822'], labIcon, 'Laboratory', [0, -0.1]);
+            const exitLayer = new L.LayerGroup([
+              addMarker(map, map.roomByName['1826'], exitIcon, 'Emergency Exit', [0.1, -1.725]),
+              addMarker(map, map.roomByName['1803'], exitIcon, 'Emergency Exit', [-0.025, 1.3]),
+            ]).addTo(map);
+
+            const baseLayers = {};
+            const overLayers = {
+              '<i class="icon ion-home"></i> You Are Here': hereLayer,
+              '<i class="icon ion-woman"></i><i class="icon ion-man"></i> Restrooms': restroomsLayers,
+              '<i class="icon ion-pizza"></i> Cafeteria': foodLayer,
+              '<i class="icon ion-erlenmeyer-flask"></i> Laboratory': labLayer,
+              '<i class="icon ion-android-exit"></i> Emergency Exit': exitLayer,
+            };
+            const panelLayers = L.control.layers(baseLayers, overLayers);
+            map.addControl(panelLayers);
+          } catch (er) {
+            console.log('Error adding layers...');
+          }
+          const legend = L.control({position: 'bottomright'});
+          legend.onAdd = () => {
+            const div = L.DomUtil.create('div', 'info legend');
+            const data = {
+              'Non-Reservable': 'lightgray',
+              'Reservable': '#80C680',
+              'Partially Available': '#FFB74D',
+              'At Capacity': '#D32F2F',
+            };
+            _.each(data, (color, label) => {
+              div.innerHTML +=
+                '<i style="background:' + color + '"></i> ' +
+                label + '<br>';
+            });
+            return div;
+          };
+          legend.addTo(map);
+        });
+      });
+    }, 30);
+  });
